a0780ab0507aec5908111e1e9cecd2bb130d4614
Dasha2018 <zavadskayadasha97@gmail.com>
2024-09-09 21:38:33 +0700

    выполнено задание JS

---
+++ HW2/script.js
@@ -0,0 +1,75 @@
+let с;
+с = 10;
+с = 20;
+alert(с);
+
+
+
+let iPhone;
+iPhone = "Год выпуска первого Iphone - 29 июня 2007 года";
+alert(iPhone);
+
+
+
+let founder;
+founder = "Имя создателя языка JavaScript - Брендан Эйх.";
+alert(founder);
+
+
+
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+
+
+let result = 2 ** 5;
+alert(result);
+
+
+
+let a = 9;
+let b = 2;
+alert(a % b);
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num = num++;
+num = num--;
+alert(num);
+
+
+
+let yearsold = prompt("Сколько вам лет?");
+alert(yearsold);
+
+
+let user = {
+    name: 'Дарья',
+    age: 27,
+    isAdmin: true
+}
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin);
+
+
+
+let name = prompt("Как тебя зовут?", "");
+alert('Привет, ' + name + '!');
+
+let number = prompt("Загадай число", 100);
+alert(number * 2);
+let f = number * 2 + 10;
+alert(f);
+let e = f / 2;
+alert(e);
+let d = e - number;
+alert(d);
+
