a1fa8fd296be86ae26effb37766a6286026bdb69
Dasha2018 <zavadskayadasha97@gmail.com>
2024-09-10 21:22:07 +0700

    Исправлена мобильная версия

---
+++ HW3/script1.js
@@ -0,0 +1,7 @@
+let password = 'пароль';
+let passwordcontent = prompt("Введите пароль");
+if (password === 1) {
+    alert("Пароль введен верно");
+} else {
+    alert("Пароль введен неправильно");
+}
